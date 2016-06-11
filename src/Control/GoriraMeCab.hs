692c71449e9b73e6164fbe1caca641e694353324
aiya000 <aiya000.develop@gmail.com>
2016-06-11 17:46:56 +0900

    Create module Data.GoriraMeCab

--- src/Control/GoriraMeCab.hs
+++ src/Control/GoriraMeCab.hs
@@ -2,49 +2,14 @@
 module Control.GoriraMeCab
   ( generateTweet
   ) where
+
+import Data.GoriraMeCab
 import Data.GoriraTwitter
-import Data.List ( concatMap, concat, foldl1 )
-import Data.List ( reverse )
+import Data.List ( concat, foldl1 )
 import Data.Text ( Text () )
 import Prelude hiding ( foldl1 )
 import System.Random.Shuffle ( shuffleM )
 import Text.MeCab ( new, parseToNodes, Node (..) )
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
-
-
-data PositionText = Begin Text | Middle Text | End Text deriving (Show)
-
-extractPositionText :: PositionText -> Text
-extractPositionText (Begin  x) = x
-extractPositionText (Middle x) = x
-extractPositionText (End    x) = x
-
-isEnd :: PositionText -> Bool
-isEnd (End _) = True
-isEnd _       = False
-
-type PositionTextCons = (Text -> PositionText)
-getPosition :: PositionText -> PositionTextCons
-getPosition (Begin  _) = Begin
-getPosition (Middle _) = Middle
-getPosition (End    _) = End
-
-
-data ChainableWords = ChainableWords PositionText PositionText deriving (Show)
-
-areChainable :: ChainableWords -> ChainableWords -> Bool
-areChainable (ChainableWords _ y1) (ChainableWords x2 _) =
-  let jointL = extractPositionText y1
-      jointR = extractPositionText x2
-  in jointL == jointR
-
-toText :: ChainableWords -> Text
-toText (ChainableWords x y) = extractPositionText x `T.append` extractPositionText y
-
-hasBegin :: ChainableWords -> Bool
-hasBegin (ChainableWords (Begin _) _) = True
-hasBegin _                            = False
 
 
 generateTweet :: Timeline -> IO TweetMessage
@@ -67,7 +32,7 @@ toChainable xs  = zipWith ChainableWords xs' (tail xs')
     xs'         = [beginWord] ++ middleWords ++ [endWord]
 
 chainWords :: [ChainableWords] -> Text
-chainWords []  = error "aho"
+chainWords []  = error "aho-baka-hoge"
 chainWords [x] = toText x
 chainWords xs  = toText . foldl1 chain . dropWhile (not . hasBegin) $ xs
   where
