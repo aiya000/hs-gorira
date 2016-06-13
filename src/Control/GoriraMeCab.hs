47cb285e51030bdfb4663eaff941b07d1404fe54
aiya000 <aiya000.develop@gmail.com>
2016-06-14 02:18:44 +0900

    Update dependency
    Exclude sub logics of Control.GoriraMecab.hs to another project
    Exclude data types of Data.GoriraMeCab to another project

--- src/Control/GoriraMeCab.hs
+++ src/Control/GoriraMeCab.hs
@@ -1,49 +1,11 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Control.GoriraMeCab
   ( generateTweet
   ) where
 
-import Data.GoriraMeCab
 import Data.GoriraTwitter
-import Data.List ( concat, foldl1 )
-import Data.Text ( Text () )
-import Prelude hiding ( foldl1 )
-import System.Random.Shuffle ( shuffleM )
-import Text.MeCab ( new, parseToNodes, Node (..) )
-import Control.Monad.IO.Class ( MonadIO (), liftIO )
-
+import Control.SentenceJP ( generateSentence )
 
 generateTweet :: Timeline -> IO TweetMessage
 generateTweet timeline = do
   let tweets = map text timeline
   generateSentence tweets
-
-generateSentence :: MonadIO m => [Text] -> m Text
-generateSentence sources = do
-  mecab      <- liftIO . new $ ["mecab"]
-  nodes      <- liftIO . mapM (parseToNodes mecab) $ sources
-  let sentences = map (toChainable . filter (/= "") . map nodeSurface) $ nodes
-  sentences' <- liftIO . shuffleM . concat $ sentences
-  return . chainWords $ sentences'
-
-toChainable :: [Text] -> [ChainableWords]
-toChainable [x] = [ChainableWords (Begin x) (End "")]
-toChainable xs  = zipWith ChainableWords xs' (tail xs')
-  where
-    beginWord   = Begin . head $ xs
-    endWord     = End   . last $ xs
-    middleWords = map Middle . init . tail $ xs
-    xs'         = [beginWord] ++ middleWords ++ [endWord]
-
-chainWords :: [ChainableWords] -> Text
-chainWords []  = error "aho-baka-hoge"
-chainWords [x] = toText x
-chainWords xs  = toText . foldl1 chain . dropWhile (not . hasBegin) $ xs
-  where
-    chain :: ChainableWords -> ChainableWords -> ChainableWords
-    chain a@(ChainableWords _ y1) b@(ChainableWords _ y2)
-      | areChainable a b = if isEnd y1
-                              then a
-                              else let l = Begin . toText $ a
-                                   in ChainableWords l y2
-      | otherwise = a
